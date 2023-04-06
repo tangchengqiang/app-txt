d592c80f5b21bc2626a7657e4f115b37e9e80603
唐成强 <2957522118@qq.com>
2023-04-06 20:42:15 +0800

    新方法

---
+++ myVueApp/src/components/a.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class=''>唐成强</div>
+</template>
+
+<script>
+import { ref, reactive, toRefs } from 'vue';
+import { useStore } from 'vuex';
+ import { useRouter, useRoute } from 'vue-router';
+ export default {
+setup(props, ctx) {
+const store = useStore();
+const router = useRouter();
+const route = useRoute();
+const data = reactive({});
+return { ...toRefs(data) };
+ },
+ };
+</script>
+
+<style lang='scss' scoped>
+
+</style>
